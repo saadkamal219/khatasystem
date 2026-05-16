7dbda72f0ed79dc748969a68de09eb14abc9c6b7
Saad Kamal <saadkamal@Saads-M2.local>
2026-05-16 18:09:55 +0600

    Update 16-05-2026

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "7mb" }));
+app.use(express.urlencoded({ limit: "7mb", extended: true }));
 
 // Routes
 app.use("/api/auth", authRoutes);
